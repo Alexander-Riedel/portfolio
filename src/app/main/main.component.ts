f0f9fd23482a3425e91728951e0e7bcdc539c529
Alexander Riedel <alexander.riedel@riedelmedia.de>
2024-11-28 00:01:22 +0100

    header, footer, landing-page, about-me

---
+++ src/app/main/main.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { LandingPageComponent } from "../landing-page/landing-page.component";
+import { AboutMeComponent } from "../about-me/about-me.component";
+
+@Component({
+  selector: 'app-main',
+  standalone: true,
+  imports: [LandingPageComponent, AboutMeComponent],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.scss'
+})
+export class MainComponent {
+
+}
