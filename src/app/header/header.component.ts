5b307a1c2dd26960c0ceb47d2cea7ebc91ca5cbf
Alexander Riedel <alexander.riedel@riedelmedia.de>
2024-12-04 22:02:24 +0100

    style update

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,16 +7,51 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss', './header.responsive.scss']
 })
+
 export class HeaderComponent {
+
+  activeSection: string = '';
   menuOpen: boolean = false;
+
+  setActiveSection(section: string): void {
+    this.activeSection = section;
+    this.menuOpen = false;
+    document.body.classList.remove('menu-open');
+  }
+
+  private scrollTimeout: any;
+
+  @HostListener('window:scroll', [])
+  onScroll(): void {
+    clearTimeout(this.scrollTimeout);
+
+    this.scrollTimeout = setTimeout(() => {
+      const sections = document.querySelectorAll('section');
+      let activeFound = false;
+
+      sections.forEach((section) => {
+        const rect = section.getBoundingClientRect();
+
+        if (rect.top <= 300 && rect.bottom >= 300) {
+          this.activeSection = section.id;
+          activeFound = true;
+        }
+      });
+
+      if (!activeFound) {
+        this.activeSection = '';
+      }
+    }, 100);
+  }
 
   toggleMenu(): void {
     this.menuOpen = !this.menuOpen;
 
     if (this.menuOpen) {
-        document.body.classList.add('menu-open');
+      document.body.classList.add('menu-open');
     } else {
-        document.body.classList.remove('menu-open');
+      document.body.classList.remove('menu-open');
     }
-}
+  }
+
 }
