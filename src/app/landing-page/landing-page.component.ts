3d1e3c4774261f91507403284889b3cca05a045f
Alexander Riedel <alexander.riedel@riedelmedia.de>
2024-12-11 23:17:41 +0100

    update style

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
-import { TranslateModule } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss', './landing-page.responsive.scss']
 })
+
 export class LandingPageComponent {
+  currentLang: string;
+
+  constructor(private translate: TranslateService) {
+    this.currentLang = this.translate.currentLang || 'de';
+
+    this.translate.onLangChange.subscribe((event) => {
+      this.currentLang = event.lang;
+    });
+  }
+
+  getLanguageClass() {
+    if (this.currentLang === 'de') {
+      return 'deutsch';
+    } else if (this.currentLang === 'en') {
+      return 'englisch';
+    } else {
+      return '';
+    }
+  }
 
 }
